1622437dc1a1e403cecf78c34831582be6001df3
sungwons <sungwonss@gmail.com>
2019-11-27 05:22:05 -0800

    Updates

---
+++ precache-manifest.48f9a7f70bfd9c01ba5965016e8e451d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db2cc886bbc716495cb14626b0a55e06",
+    "url": "/React_MovieApp/index.html"
+  },
+  {
+    "revision": "cbca1a1970886711e73e",
+    "url": "/React_MovieApp/static/js/2.d330bac6.chunk.js"
+  },
+  {
+    "revision": "8eae75a1eaf6fe190c60",
+    "url": "/React_MovieApp/static/js/main.085f9ce9.chunk.js"
+  },
+  {
+    "revision": "b91d62a75fcc27ce8fae",
+    "url": "/React_MovieApp/static/js/runtime-main.d5911508.js"
+  }
+]);
